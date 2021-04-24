83c7cfb92bd6d20ca6a736278b53dd9ca01d7ff2
James Kelly <james@hicag.org>
2021-04-24 10:41:03 -0500

    Description shouldn't start with an article

--- Formula/awsctx.rb
+++ Formula/awsctx.rb
@@ -1,5 +1,5 @@
 class Awsctx < Formula
-  desc "A cli tool to switch AWS profiles"
+  desc "Cli tool to switch AWS profiles"
   homepage "https://github.com/dtennander/awsctx"
   url "https://github.com/dtennander/awsctx/archive/v1.3.tar.gz"
   sha256 "659184344bf4d6a7ab25737064d9a336b9bb7fdbc95fad028d84f3dbdd8c545c"
