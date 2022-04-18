29014f987744a6059b8be881b91f9db93d06d904
James Kelly <james@hicag.org>
2022-04-18 11:42:02 -0500

    ayusin 0.2.0_1
    Depend on fping

--- Formula/ayusin.rb
+++ Formula/ayusin.rb
@@ -3,9 +3,11 @@ class Ayusin < Formula
   homepage "https://github.com/jamebus/ayusin"
   url "https://github.com/jamebus/ayusin/archive/v0.2.0.tar.gz"
   sha256 "daf56d57c0ccd139022fa46d724bc4a1f7ec287997edef029f356743e153c5d7"
+  revision 1
   head "https://github.com/jamebus/ayusin.git"
 
   depends_on "debianutils"
+  depends_on "fping"
 
   def install
     system "make", "prefix=#{prefix}", "install"
