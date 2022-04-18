cce5a6767f9ef5d665a7229d0953ffeb80fa895e
James Kelly <james@hicag.org>
2022-04-18 11:44:52 -0500

    ayusin 0.2.0_2
    Depend on mas

--- Formula/ayusin.rb
+++ Formula/ayusin.rb
@@ -3,11 +3,12 @@ class Ayusin < Formula
   homepage "https://github.com/jamebus/ayusin"
   url "https://github.com/jamebus/ayusin/archive/v0.2.0.tar.gz"
   sha256 "daf56d57c0ccd139022fa46d724bc4a1f7ec287997edef029f356743e153c5d7"
-  revision 1
+  revision 2
   head "https://github.com/jamebus/ayusin.git"
 
   depends_on "debianutils"
   depends_on "fping"
+  depends_on "mas"
 
   def install
     system "make", "prefix=#{prefix}", "install"
