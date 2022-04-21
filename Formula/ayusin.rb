edfb5f647609ec9db2ff679019140889edf45749
James Kelly <james@hicag.org>
2022-04-20 19:24:39 -0500

    ayusin 0.2.1

--- Formula/ayusin.rb
+++ Formula/ayusin.rb
@@ -1,9 +1,8 @@
 class Ayusin < Formula
   desc "Automated housekeeping tasks"
   homepage "https://github.com/jamebus/ayusin"
-  url "https://github.com/jamebus/ayusin/archive/v0.2.0.tar.gz"
-  sha256 "daf56d57c0ccd139022fa46d724bc4a1f7ec287997edef029f356743e153c5d7"
-  revision 2
+  url "https://github.com/jamebus/ayusin/archive/v0.2.1.tar.gz"
+  sha256 "c6f449a5c6c25ffc63d938acf5611c5b58d97331adce2a6727471851348f7d1a"
   head "https://github.com/jamebus/ayusin.git"
 
   depends_on "debianutils"
