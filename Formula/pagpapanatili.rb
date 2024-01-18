5649fd44555a4025f272c15a0d211c1d84225c64
James Kelly <james@hicag.org>
2024-01-17 22:09:16 -0600

    pagpapanatili 0.2.2_1 (fix dependencies)

--- Formula/pagpapanatili.rb
+++ Formula/pagpapanatili.rb
@@ -3,13 +3,15 @@ class Pagpapanatili < Formula
   homepage "https://github.com/jamebus/pagpapanatili"
   url "https://github.com/jamebus/pagpapanatili/archive/refs/tags/v0.2.2.tar.gz"
   sha256 "fde460290d3bbef685e79c7ba00958771a0226017b8a169a34d5d7695c82a69c"
+  revision 1
   head "https://github.com/jamebus/pagpapanatili.git", branch: "main"
 
   depends_on "awscli"
+  depends_on "creativeprojects/tap/resticprofile"
   depends_on "gum"
   depends_on "restic"
-  depends_on "resticprofile"
-  depends_on "ayusin" => :optional
+  depends_on "yq"
+  depends_on "jamebus/tools/ayusin" => :optional
 
   def install
     system "make", "prefix=#{prefix}", "install"
