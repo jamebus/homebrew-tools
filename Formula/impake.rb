857e3944ad50c49000dfa762ab750089ec2bcf19
James Kelly <james@hicag.org>
2024-12-28 17:56:15 -0600

    Correct brew audit problems in impake

--- Formula/impake.rb
+++ Formula/impake.rb
@@ -1,14 +1,14 @@
 class Impake < Formula
   desc "Easy ephemeral file sharing"
   homepage "https://github.com/jamebus/impake"
-  url "https://github.com/jamebus/impake/archive/v1.0.2.tar.gz"
+  url "https://github.com/jamebus/impake/archive/refs/tags/v1.0.2.tar.gz"
   sha256 "fbc9ce4a065e8da4079868e322733aa6ee654db219a05098d0ba9789d17d6fb0"
-  head "https://github.com/jamebus/impake.git"
+  head "https://github.com/jamebus/impake.git", branch: "master"
 
-  depends_on "p7zip"
   depends_on "jq"
-  depends_on "jamebus/tools/realpath-osx" => :optional
+  depends_on "p7zip"
   depends_on "coreutils"                  => :optional
+  depends_on "jamebus/tools/realpath-osx" => :optional
 
   def install
     bin.install "impake"
@@ -16,7 +16,7 @@ class Impake < Formula
 
   def caveats
     "You'll need a realpath(1) command in your $PATH for impake to work.\n" \
-    "You can install it from jamebus/tools/realpath-osx or coreutils."
+      "You can install it from jamebus/tools/realpath-osx or coreutils."
   end
 
   test do
