fbc505e2a4cae0ee7b1c84214469669209afc702
James Kelly <james@hicag.org>
2025-05-04 11:17:28 +0800

    kalye 0.2.0

--- Formula/kalye.rb
+++ Formula/kalye.rb
@@ -1,8 +1,8 @@
 class Kalye < Formula
   desc "Quickly find your IP address"
   homepage "https://github.com/jamebus/kalye"
-  url "https://github.com/jamebus/kalye/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "d0b98403d5da09de71005208a1980e26e5b2e2f36f7d68436ee4847a53d8685a"
+  url "https://github.com/jamebus/kalye/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "0b03783ece74065c388e25f7c116426ab76eb21bcb9c64a8102a1788b9c12461"
   head "https://github.com/jamebus/kalye.git", branch: "main"
 
   depends_on "gum"
