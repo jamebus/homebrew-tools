0d623d9d37954ead8a3a7d5407885d6db6fcfe91
James Kelly <james@hicag.org>
2026-03-07 09:03:50 -0600

    marklassian-cli 1.1.0

--- Formula/marklassian-cli.rb
+++ Formula/marklassian-cli.rb
@@ -1,8 +1,8 @@
 class MarklassianCli < Formula
   desc "Command-line wrapper for marklassian"
   homepage "https://github.com/jamebus/marklassian-cli"
-  url "https://github.com/jamebus/marklassian-cli/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "eb548de7a0c03a217970f4f380793571d860e6bd97194483772fc3f8f076b8ad"
+  url "https://github.com/jamebus/marklassian-cli/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "c197afb89be44df0ceff13db7fc8909e0c3a318fd5d22dc4b92dbd7f82aaa437"
   head "https://github.com/jamebus/marklassian-cli.git", branch: "main"
 
   depends_on "node"
