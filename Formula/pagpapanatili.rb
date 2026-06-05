c6aa9def09da409cbdf06cffac396c6df37f6498
James Kelly <james@hicag.org>
2026-06-04 19:33:27 -0500

    pagpapanatili should depend on resticprofile in core

--- Formula/pagpapanatili.rb
+++ Formula/pagpapanatili.rb
@@ -3,12 +3,13 @@ class Pagpapanatili < Formula
   homepage "https://github.com/jamebus/pagpapanatili"
   url "https://github.com/jamebus/pagpapanatili/archive/refs/tags/v2.0.5.tar.gz"
   sha256 "6ae41ddc21419f93518e45828aaebd8ceeaded67465e18796584e6dc9a114080"
+  revision 1
   head "https://github.com/jamebus/pagpapanatili.git", branch: "main"
 
   depends_on "awscli"
-  depends_on "creativeprojects/tap/resticprofile"
   depends_on "gum"
   depends_on "restic"
+  depends_on "resticprofile"
   depends_on "yq"
   depends_on "jamebus/tools/ayusin" => :optional
 
